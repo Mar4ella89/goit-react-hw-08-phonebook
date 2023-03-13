b1a97dd2fe8e5409f9eef3a77124dd187128867a
Mar4ella89 <103376023+Mar4ella89@users.noreply.github.com>
2023-03-13 16:03:38 +0000

    Deploying to gh-pages from @ Mar4ella89/goit-react-hw-08-phonebook@f477738af82956a38bfc0c942bb3f42dc5c2c87a 🚀

--- static/js/472.a438badf.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[472],{2472:function(e,n,o){o.r(n);var t=o(184);n.default=function(){return(0,t.jsx)("div",{children:"Login"})}}}]);
-//# sourceMappingURL=472.a438badf.chunk.js.map
