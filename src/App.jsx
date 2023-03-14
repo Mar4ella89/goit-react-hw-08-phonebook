18ced49c83e4f29f8871f25b0f1ff231a1604100
Marat Baikin <3371335@gmail.com>
2023-03-14 10:34:05 +0200

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
-
-import AuthLayout from 'modules/AuthLayout/AuthLayout';
 
 import PrivateRoute from 'modules/PrivateRoute/PrivateRoute';
 import PublicRoute from 'modules/PublicRoute/PublicRoute';
@@ -13,23 +11,19 @@ const RegisterPage = lazy(() => import('./Pages/RegisterPage/RegisterPage'));
 
 export const App = () => {
   return (
-    <AuthLayout>
-      <BrowserRouter basename="/goit-react-hw-08-phonebook/">
-        <Suspense>
-          <Navbar />
-          <Routes>
-            <Route element={<PrivateRoute />}>
-              <Route path="/contacts" element={<ContactBooks />} />
-            </Route>
-            <Route element={<PublicRoute />}>
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
-            </Route>
+    <Suspense>
+      <Navbar />
+      <Routes>
+        <Route element={<PrivateRoute />}>
+          <Route path="/contacts" element={<ContactBooks />} />
+        </Route>
+        <Route element={<PublicRoute />}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
 
-            <Route path="*" element={<LoginPage />} />
-          </Routes>
-        </Suspense>
-      </BrowserRouter>
-    </AuthLayout>
+        <Route path="*" element={<LoginPage />} />
+      </Routes>
+    </Suspense>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
 
 import { store, persistor } from 'redux/store';
+import AuthLayout from 'modules/AuthLayout/AuthLayout';
 
 import { App } from 'App';
 import './index.css';
@@ -12,7 +14,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <AuthLayout>
+          <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+            <App />
+          </BrowserRouter>
+        </AuthLayout>
       </PersistGate>
     </Provider>
   </React.StrictMode>
